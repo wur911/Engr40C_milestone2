6c0fed9ea3c395b0163d42b3b382ceddbecc28a2
Raymond Wu <wur911@stanford.edu>
2014-05-11 17:23:20 -0700

    Updated get_frame

--- channel_coding.py
+++ channel_coding.py
@@ -14,7 +14,9 @@ def get_frame(databits, cc_len):
     Add header and also apply Hamming code of n=3
     Return the resulted frame (channel-coded header and databits)
     '''
-    
+    index,payload = encode(databits,cc_len)
+    header = get_header(payload,index)
+    frame = header+payload
     return frame
     
 def get_header(payload, index):
@@ -23,7 +25,7 @@ def get_header(payload, index):
     Do not confuse this with header from source module.
     Communication system use layers and attach nested headers from each layers 
     '''
-
+    
     return header
     
 def encode(databits, cc_len):
@@ -43,8 +45,30 @@ def get_databits(recd_bits):
     Parse the header and perform channel decoding.
     Note that header is also channel-coded    
     '''
-
+    header = recd_bits[:6]
+    coded_bits = recd_bits[6:]
+    index = parse_header
+    databits = decode(coded_bits,index)
     return databits
+
+def parse_header(header):
+    '''
+    Parse the header received from channel-decoded bits
+    Use a (3,1,3) Hamming code.
+    '''
+    first_bit = header[0]+header[1]+header[2]
+    second_bit = header[3]+header[4]+header[5]
+    if first_bit < 2 and second_bit < 2:
+        index = 0
+    elif first_bit < 2 and second_bit >= 2:
+        index = 1
+    elif first_bit >= 2 and second_bit < 2:
+        index = 2
+    elif first_bit >= 2 and second_bit >=2:
+        index = 3
+    else:
+        index = -1
+    return index
 
 def decode(coded_bits, index):
     '''
